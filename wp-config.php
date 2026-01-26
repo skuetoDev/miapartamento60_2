bd12ba4c46738e0bcd6adf6713c05a3e560eb424
skuetoDev <mariogn1987@gmail.com>
2026-01-26 23:00:03 +0100

    navbar done

--- wp-config.php
+++ wp-config.php
@@ -23,19 +23,19 @@ define( 'WP_CACHE', true );
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'u489371835_m1qzU' );
+define( 'DB_NAME', 'miapartamento60DB' );
 
 /** Database username */
-define( 'DB_USER', 'u489371835_58KEz' );
+define( 'DB_USER', 'root' );
 
 /** Database password */
-define( 'DB_PASSWORD', '9RMFYj0bGc' );
+define( 'DB_PASSWORD', '' );
 
 /** Database hostname */
-define( 'DB_HOST', '127.0.0.1' );
+define( 'DB_HOST', 'localhost' );
 
 /** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8' );
+define( 'DB_CHARSET', 'utf8mb4' );
 
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
---
+++ wp-content/themes/temaPersonalizado/header-page.php
@@ -0,0 +1,42 @@
+<!DOCTYPE HTML>
+<!--
+    Dopetrope by HTML5 UP
+    html5up.net | @ajlkn
+    Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html <?php language_attributes(); ?>>
+      <head>
+        <title><?php echo bloginfo('name'); ?></title>
+        <meta charset="<?php bloginfo('charset'); ?>"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
+        <!-- para importar estilos desde functions.php -->
+        <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.ico" type="image/x-icon"/>
+        <?php wp_head(); ?>
+      </head>
+      <div id="page-wrapper">
+         <!-- Header -->
+         <section id="header">
+            <div class="logo">
+               <img class="icon" src="wp-content\uploads\2026\01\logo-150x150.png" alt="Logo-miapartamento60"/>
+
+            </div>
+            <!-- Logo -->
+            <h1>
+               <a href="<?php echo home_url(); ?>">
+                  <span>Mi</span>Apartamento<span>60</span>
+               </a>
+            </h1>
+            <h2><?php echo bloginfo('description'); ?></h2>
+               <!-- Nav -->
+               <section id="nav">
+                  <?php
+                     $arg = array(
+                        'theme_location' => 'principal',
+                        'container' => 'nav',
+                        'container_id' => 'nav'
+                     );
+                     wp_nav_menu($arg);
+                        ?>
+               </section>
+            </section>
+         </div>
