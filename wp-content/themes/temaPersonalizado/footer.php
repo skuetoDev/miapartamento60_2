5ece4cbadd6c3b17ad07602ad2f8adf02a26058d
skuetoDev <mariogn1987@gmail.com>
2026-01-29 23:11:08 +0100

    footer 3/4

--- wp-content/themes/temaPersonalizado/footer.php
+++ wp-content/themes/temaPersonalizado/footer.php
@@ -3,26 +3,7 @@
    <div class="container">
       <div class="row">
          <div class="col-4 col-12-medium">
-            <section>
-               <!-- para llamar a las register_sidebar en functions.php -->
-               <ul class="divided">
-                     <?php
-                     dynamic_sidebar('footer ultimas entradas');
-                        ?>
-               </ul>
-            </section>
-         </div>
-         <div class="col-4 col-6-medium col-12-small">
-            <section>
-               <ul class="divided">
-                  <?php
-                        dynamic_sidebar('categorias');
-                    ?>
-               </ul>
-            </section>
-         </div>
-         <div class="col-4 col-12-medium">
-            <section >
+            <section class="social-media-section">
                <header class="social-media-header">
                   <h2>Redes Sociales</h2>
                </header>
@@ -34,29 +15,20 @@
                      >
                   </li>
                   <li>
-                     <a class="icon fa-instagram" href="https://www.instagram.com/scrap.lion/" target="_blank"
-                        ><span class="label">Instagram</span></a
+                     <a class="icon fa-facebook" 
+                        href="https://www.facebook.com/profile.php?id=100080465887995" target="_blank"
+                        ><span class="label">Facebook</span></a
                      >
                   </li>
                   <li>
-                    <a class="icon icon-tiktok" href="https://www.tiktok.com/@scrap.lion" target="_blank">
-                       <span class="label">TikTok</span>
-                       <img class="tiktok" src="<?php echo get_template_directory_uri();
-                        ?>/images/tiktok1.svg" alt="TikTok" />
-                    </a>
-                  </li>
-                  <li>
-                     <a class="icon fa-youtube" href="https://www.youtube.com/@scraplion2758" target="_blank"
-                        ><span class="label">YouTube</span></a
+                     <a class="icon fa-facebook" 
+                        href="https://www.facebook.com/profile.php?id=100080465887995" target="_blank"
+                        ><span class="label">Facebook</span></a
                      >
                   </li>
-                  <li>
-                    <a class="icon icon-twitch" href="https://www.twitch.tv/scraplion" target="_blank">
-                       <span class="label">Twitch</span>
-                       <img class="twitch" src="<?php echo get_template_directory_uri();
-                        ?>/images/twitch.svg" alt="Twitch" />
-                    </a>
-                  </li>
+
+               
+                
                </ul>
             </section>
          </div>
@@ -67,9 +39,9 @@
                   <li>Developed by  <a href='https://www.linkedin.com/in/skuetoDev/' target="_blank">
                      SkuetoDev.</a> 
                   </li>
-                  <li></a>&copy;2025 
+                  <li></a>&copy;2026 
                      <a href='https://www.instagram.com/scrap.lion/' target="_blank">
-                     Scraplion
+                     MiApartamento60
                      </a>.All rights reserved.</li>
                </ul>
             </div>
